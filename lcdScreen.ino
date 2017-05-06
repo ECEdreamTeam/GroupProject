2c769eb85588de6067b0980ead9adbaa58d10871
ECEdreamTeam <ecedreamteam@gmail.com>
2017-05-06 17:24:32 -0400

    Add files via upload

---
+++ lcdScreen.ino
@@ -0,0 +1,29 @@
+
+#include <LiquidCrystal.h>
+
+// initialize the library with the numbers of the interface pins
+LiquidCrystal lcd(7, 9, 5, 4, 3, 8);
+
+int pin = 2;
+
+void setup() {
+  Serial.begin(9600);
+  pinMode(pin, INPUT);
+  // set up the LCD's number of columns and rows: 
+  lcd.begin(16, 2);  
+}
+
+void loop() {
+  
+  Serial.println(digitalRead(pin));
+  
+  if (digitalRead(pin)==1 ){
+    lcd.clear();
+    lcd.print("Door Open");
+  }
+ 
+  else if (digitalRead(pin) == 0) {
+    lcd.clear();
+    lcd.print("Door Closed");
+  }
+}
---
+++ pipe1.ino
@@ -0,0 +1,99 @@
+#include <SPI.h>
+#include "RF24.h"
+
+RF24 radio(7,8);
+
+const uint64_t pipe1 = 0xE8E8F0F0E1LL;
+const int trigPin = 4;
+const int echoPin = 5;
+
+int data;
+int doorPin = 3;
+
+
+void setup() {
+  Serial.begin(9600);
+  radio.begin();
+  radio.setChannel(108); 
+  radio.openWritingPipe(pipe1); 
+  pinMode(trigPin, OUTPUT);
+  pinMode(echoPin, INPUT);
+  pinMode(doorPin, OUTPUT);
+}
+
+void loop() {
+  long duration, inches, cm;
+  
+  digitalWrite(trigPin, LOW);
+  delayMicroseconds(2);
+  digitalWrite(trigPin, HIGH);
+  delayMicroseconds(10);
+  digitalWrite(trigPin, LOW);
+
+  duration = pulseIn(echoPin, HIGH);
+  inches = microsecondsToInches(duration);
+
+  if (inches < 56 && data == 1){
+     data = 1;
+     
+  Serial.print(inches);
+  Serial.print("in, ");
+  Serial.print(data);
+  Serial.print(" data");
+  Serial.println();
+  digitalWrite(doorPin, HIGH);
+
+  delay(100);
+
+  }
+   
+  else if (inches < 56 && data !=1){
+    data = 1;
+    
+  radio.write(&data, sizeof(data)); 
+  Serial.print(inches);
+  Serial.print("in, ");
+  Serial.print(data);
+  Serial.print(" data");
+  Serial.println();
+  digitalWrite(doorPin, HIGH);
+
+  delay(100);
+
+  }
+  else if (inches >= 56 && data !=0){
+    data = 0;
+    
+  radio.write(&data, sizeof(data));
+  Serial.print(inches);
+  Serial.print("in, ");
+  Serial.print(data);
+  Serial.print(" data");
+  Serial.println();
+  digitalWrite(doorPin, LOW);
+
+  delay(100);
+
+  }
+  else{
+    data = 0;
+   
+  Serial.print(inches);
+  Serial.print("in, ");
+  Serial.print(data);
+  Serial.print(" data");
+  Serial.println();
+  digitalWrite(doorPin, LOW);
+
+  delay(100);
+  }
+  
+}
+
+ long microsecondsToInches(long microseconds)
+{
+  return microseconds / 74 / 2;
+}
+
+
+
